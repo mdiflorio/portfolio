220a873abb63cd28d98d126d3483aebb9c179046
Madhava Di Florio <mattdiflorio@gmail.com>
2018-11-11 18:19:55 +0100

    Added small contacts page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,9 +4,14 @@ class Contact extends Component {
   render() {
     return (
       <div class="container">
-        <div class="about">
+        <div class="content-container contact">
           <h2>Contact me</h2>
-          <p />
+          <p>
+            The easiest way to get in contact is by{" "}
+            <a href="mailto:madhava.diflorio@gmail.com">email.</a> If that
+            doesn't suit you feel free to reach out through one of my other
+            social media accounts.
+          </p>
         </div>
       </div>
     );
