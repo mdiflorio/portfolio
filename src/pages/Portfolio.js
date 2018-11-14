f00412ba66c0a289d95624c8e8d36e5e2f2c7578
Madhava Di Florio <mattdiflorio@gmail.com>
2018-11-14 08:48:11 +0100

    Added carousel functionality to portfolio items.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Modal from "react-responsive-modal";
+import Slider from "react-slick";
 
 import { portfolioContent } from "../content/portfolio";
 
@@ -26,7 +27,7 @@ class Portfolio extends Component {
         {portfolioContent.map(item => (
           <div className="item">
             <button onClick={this.onOpenModal.bind(null, item)}>
-              <img src={item.img_src} alt={item.title} />
+              <img src={item.imgs[0]} alt={item.title} />
             </button>
           </div>
         ))}
@@ -36,10 +37,28 @@ class Portfolio extends Component {
 }
 
 const SelectedItemContent = ({ item }) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true
+  };
   return (
-    <div>
-      <h2>{item.title}</h2>
-      <p>{item.desc}</p>
+    <div className="modalContainer">
+      <Slider {...settings} className="slider">
+        {Object.keys(item).length > 0 &&
+          item.imgs.map(image => (
+            <div>
+              <img src={image} />
+            </div>
+          ))}
+      </Slider>
+      <div>
+        <h2>{item.title}</h2>
+        <p>{item.desc}</p>
+      </div>
     </div>
   );
 };
