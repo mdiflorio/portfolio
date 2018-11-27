af37d129666cdf87fc9ec6b9b196ac74c34483de
Madhava Di Florio <mattdiflorio@gmail.com>
2018-11-27 20:49:01 +0100

    Added images for uconnected portfolio piece.

--- src/content/portfolio/index.js
+++ src/content/portfolio/index.js
@@ -1,11 +1,14 @@
 export const portfolioContent = [
   {
-    title: "Simon Game",
+    title: "uConnected App",
     imgs: [
-      require("../../assets/gameoflife-SS.jpg"),
-      require("../../assets/gameoflife-SS.jpg"),
-      require("../../assets/gameoflife-SS.jpg"),
-      require("../../assets/gameoflife-SS.jpg")
+      require("../../assets/uconnected/1.png"),
+      require("../../assets/uconnected/2.png"),
+      require("../../assets/uconnected/7.png"),
+      require("../../assets/uconnected/6.png"),
+      require("../../assets/uconnected/3.png"),
+      require("../../assets/uconnected/4.png"),
+      require("../../assets/uconnected/5.png")
     ],
     desc:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac metus ut mi vulputate sollicitudin. Pellentesque auctor interdum pulvinar. Suspendisse vitae venenatis libero. Mauris hendrerit lacus ac ligula luctus porta. Integer accumsan ullamcorper sollicitudin. Praesent auctor urna sit amet auctor sollicitudin. Suspendisse eu nulla eleifend, euismod velit sed, cursus nibh. Duis blandit quis est eget blandit. Etiam a est ac diam placerat efficitur et ac elit. Sed ac vulputate leo. Maecenas augue elit, dignissim id enim ac, cursus luctus risus. Vestibulum luctus molestie libero, ut tristique augue porttitor eget. Donec a nisl commodo urna accumsan fermentum ac non orci. Vivamus est eros, pretium et hendrerit ac, ornare ac magna. Vivamus vitae mauris eu enim tincidunt dictum. Cras non elit vitae justo aliquam sagittis."
@@ -33,7 +36,7 @@ export const portfolioContent = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac metus ut mi vulputate sollicitudin. Pellentesque auctor interdum pulvinar. Suspendisse vitae venenatis libero. Mauris hendrerit lacus ac ligula luctus porta. Integer accumsan ullamcorper sollicitudin. Praesent auctor urna sit amet auctor sollicitudin. Suspendisse eu nulla eleifend, euismod velit sed, cursus nibh. Duis blandit quis est eget blandit. Etiam a est ac diam placerat efficitur et ac elit. Sed ac vulputate leo. Maecenas augue elit, dignissim id enim ac, cursus luctus risus. Vestibulum luctus molestie libero, ut tristique augue porttitor eget. Donec a nisl commodo urna accumsan fermentum ac non orci. Vivamus est eros, pretium et hendrerit ac, ornare ac magna. Vivamus vitae mauris eu enim tincidunt dictum. Cras non elit vitae justo aliquam sagittis."
   },
   {
-    title: "Simon Game",
+    title: "Thatisit!",
     imgs: [
       require("../../assets/gameoflife-SS.jpg"),
       require("../../assets/gameoflife-SS.jpg"),
@@ -44,7 +47,7 @@ export const portfolioContent = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac metus ut mi vulputate sollicitudin. Pellentesque auctor interdum pulvinar. Suspendisse vitae venenatis libero. Mauris hendrerit lacus ac ligula luctus porta. Integer accumsan ullamcorper sollicitudin. Praesent auctor urna sit amet auctor sollicitudin. Suspendisse eu nulla eleifend, euismod velit sed, cursus nibh. Duis blandit quis est eget blandit. Etiam a est ac diam placerat efficitur et ac elit. Sed ac vulputate leo. Maecenas augue elit, dignissim id enim ac, cursus luctus risus. Vestibulum luctus molestie libero, ut tristique augue porttitor eget. Donec a nisl commodo urna accumsan fermentum ac non orci. Vivamus est eros, pretium et hendrerit ac, ornare ac magna. Vivamus vitae mauris eu enim tincidunt dictum. Cras non elit vitae justo aliquam sagittis."
   },
   {
-    title: "Simon Game",
+    title: "Ketboard",
     imgs: [
       require("../../assets/gameoflife-SS.jpg"),
       require("../../assets/gameoflife-SS.jpg"),
@@ -55,7 +58,7 @@ export const portfolioContent = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac metus ut mi vulputate sollicitudin. Pellentesque auctor interdum pulvinar. Suspendisse vitae venenatis libero. Mauris hendrerit lacus ac ligula luctus porta. Integer accumsan ullamcorper sollicitudin. Praesent auctor urna sit amet auctor sollicitudin. Suspendisse eu nulla eleifend, euismod velit sed, cursus nibh. Duis blandit quis est eget blandit. Etiam a est ac diam placerat efficitur et ac elit. Sed ac vulputate leo. Maecenas augue elit, dignissim id enim ac, cursus luctus risus. Vestibulum luctus molestie libero, ut tristique augue porttitor eget. Donec a nisl commodo urna accumsan fermentum ac non orci. Vivamus est eros, pretium et hendrerit ac, ornare ac magna. Vivamus vitae mauris eu enim tincidunt dictum. Cras non elit vitae justo aliquam sagittis."
   },
   {
-    title: "Simon Game",
+    title: "SAuf",
     imgs: [
       require("../../assets/gameoflife-SS.jpg"),
       require("../../assets/gameoflife-SS.jpg"),
